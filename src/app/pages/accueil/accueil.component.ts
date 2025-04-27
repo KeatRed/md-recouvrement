75e0b22cf24d84f8518d3f919ffe28dd44e75ac7
KEAT <keatnns@gmail.com>
2025-04-27 23:42:30 +0200

    MAJ modal et missions 2

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -16,12 +16,92 @@ export class AccueilComponent {
     {
       title: 'Vous êtes une entreprise',
       content: `
-        <p>Le recouvrement de créances est une étape essentielle pour maintenir la santé financière des entreprises...</p>
+<section class="presentation-mdrecouvrement">
+  <p>
+    <strong>Le Cabinet MDrecouvrement</strong> accompagne les petites et moyennes entreprises dans la gestion de leurs impayés clients, en privilégiant une approche amiable, efficace et respectueuse.
+    Nous intervenons principalement dans les départements du Val-d'Oise (95), de l’Oise (60) ainsi que sur la plateforme de Roissy (93/77), au cœur d’un tissu économique dynamique.
+  </p>
+
+  <p>Notre offre s’adresse aux :</p>
+  <ul>
+    <li>Entreprises du secteur des services</li>
+    <li>Artisans</li>
+    <li>Professions libérales</li>
+    <li>Garages</li>
+    <li>Établissements scolaires</li>
+    <li>Bailleurs</li>
+    <li>Etc...</li>
+  </ul>
+
+  <p><strong>Structures locales confrontées à des retards ou défauts de paiement.</strong></p>
+
+  <h3>Ce qui nous distingue :</h3>
+  <ul>
+    <li><strong>Réactivité et efficacité :</strong> Une prise en charge immédiate des dossiers pour une résolution rapide.</li>
+    <li><strong>Zéro risque financier :</strong> Un modèle de rémunération basé sur le succès, sans avance de frais pour vos clients.</li>
+    <li><strong>Outils digitaux et transparence :</strong> Un suivi en temps réel des dossiers pour un contrôle total du recouvrement.</li>
+  </ul>
+
+  <p><a href="#en-savoir-plus">En savoir plus</a></p>
+
+  <section id="en-savoir-plus">
+    <p>
+      L’objectif du Cabinet MDrecouvrement est d’accompagner les TPE, PME, garages, écoles privées, bailleurs, sous-traitants etc. de notre région dans la récupération de leurs impayés, grâce à une solution simple, efficace, rapide et professionnelle avec une démarche RSE :
+    </p>
+
+    <ul>
+      <li>Développement durable avec le moins d’impressions possibles</li>
+      <li>Politique de recrutement</li>
+      <li>Démarche de relances respectueuses des débiteurs - sans détériorer la relation client</li>
+      <li>Une gestion sur mesure adaptée à chaque secteur d'activité</li>
+      <li>Une tarification transparente et sans engagement</li>
+    </ul>
+
+    <p>
+      Le cabinet MDrecouvrement propose un service local, avec des frais de dossiers forfaitaires initiaux de <strong>15€</strong> afin de couvrir les frais fixes de base (un appel, un SMS, un mail, une lettre RAR et une lettre simple) puis une rémunération de nos services basée sur des tarifs uniquement au résultat.
+    </p>
+
+    <p>
+      Nous proposons également la gestion ainsi que le suivi des dossiers en contentieux en cas d’échec de l’amiable à la demande du client et contre rémunération spécifique.
+    </p>
+
+    <h3>Notre politique tarifaire et nos atouts :</h3>
+
+    <p><strong>Nos honoraires (hors frais de dossier) ne sont facturés qu’en cas de succès !</strong></p>
+
+    <h4>Recouvrement amiable :</h4>
+    <ul>
+      <li>Frais de dossier de <strong>15€ HT</strong></li>
+      <li><strong>Honoraires :</strong>
         <ul>
-          <li>Nous ne sommes pas des huissiers.</li>
-          <li>Notre méthode repose sur la négociation et l'accompagnement.</li>
+          <li>10 % des sommes encaissées pour les créances de moins de 6 mois</li>
+          <li>15 % pour les créances de plus de 6 mois</li>
         </ul>
-        <p>Notre rôle est complémentaire et stratégique.</p>
+      </li>
+    </ul>
+
+    <h4>Recouvrement contentieux :</h4>
+    <ul>
+      <li>Frais de dossier de <strong>15€ HT</strong> (sauf si dossier ouvert initialement en amiable)</li>
+      <li>Frais de gestion contentieux de <strong>150€ HT</strong> (obtention du titre exécutoire, hors frais de greffe et de commissaire de justice)</li>
+      <li><strong>Honoraires :</strong>
+        <ul>
+          <li>10 % des sommes encaissées pour les créances de moins de 6 mois</li>
+          <li>15 % des sommes encaissées pour les créances de plus de 6 mois</li>
+        </ul>
+      </li>
+    </ul>
+
+    <p><strong>Nos honoraires sont déduits avant le reversement des montants récupérés sur les créances et les fonds récupérés vous sont reversés sous 48h afin d’optimiser votre trésorerie.</strong></p>
+
+    <h4>Passage à l’adresse du débiteur (au choix du client) :</h4>
+    <ul>
+      <li>Forfait <strong>50€ HT</strong> par passage</li>
+      <li>Frais kilométriques pris en charge par le client</li>
+    </ul>
+  </section>
+</section>
+
       `
     },
     {
@@ -66,7 +146,6 @@ export class AccueilComponent {
     <ul>
       <li><strong>L'anticipation des risques :</strong> identifier les signaux précurseurs d'impayés et mettre en place des stratégies</li>
       <li><strong>Les méthodes de relances amiables internes :</strong> techniques efficaces et cordiales</li>
-      <li><strong>Le recouvrement juridique :</strong> étapes et procédures du recouvrement judiciaire</li>
       <li><strong>La préparation à l'externalisation :</strong> choisir le bon prestataire et collaborer efficacement</li>
     </ul>
   </section>
--- src/app/pages/missions/missions.component.ts
+++ src/app/pages/missions/missions.component.ts
@@ -13,42 +13,56 @@ export class MissionsComponent {
     {
       title: 'RECOUVREMENT AMIABLE DE VOS FACTURES',
       description: 'Gestion efficace de vos créances. Nous transformons vos impayés en liquidités rapidement.',
-      title2: '',
-      description2: '',
-      title3: '',
-      description3: '',
-      title4: '',
-      description4: '',
+      html: `<p>Gestion efficace de vos créances. Nous transformons vos impayés en liquidités rapidement.</p>`,
       background: 'assets/facture.webp'
     },
     {
       title: 'RECOUVREMENT CONTENTIEUX DE VOS FACTURES',
       description: 'Procédures judiciaires pour garantir le paiement de vos créances. Action rapide et sécurisée.',
-      title2: '',
-      description2: '',
-      title3: '',
-      description3: '',
-      list : [ ],
-      title4: '',
-      description4: '',
-      background: '/assets/dossier.webp'
+      html: `<p>Procédures judiciaires pour garantir le paiement de vos créances. Action rapide et sécurisée.
+      Quand la voie amiable n’aboutit pas, nous passons à l’étape supérieure.
+      Chez Cabinet MDrecouvrement, nous privilégions toujours la négociation et la résolution amiable. Toutefois, lorsqu’un débiteur reste sourd à nos démarches malgré nos relances, il devient nécessaire d’engager une procédure judiciaire pour faire valoir vos droits.
+      Dépôt de la requête en injonction de payer
+      Notre mission dans cette phase contentieuse est de :
+      Constituer le dossier juridique complet nécessaire à la procédure.
+      Déposer pour votre compte la requête d’injonction de payer auprès du tribunal compétent.
+      Assurer le suivi de la procédure jusqu’à l’obtention d’un titre exécutoire, un document officiel qui permet d’engager les mesures de recouvrement forcé (saisie sur compte bancaire, saisie sur salaire, etc.).
+      Un accompagnement de A à Z
+      Vous restez informé à chaque étape. Nous collaborons avec des huissiers de justice et, si nécessaire, des avocats partenaires pour faire exécuter la décision de justice.
+      Nos honoraires (hors frais de dossier) ne sont facturés qu’en cas de succès ! 
+Recouvrement contentieux : 
+Frais de dossier de 15€ HT (sauf si dossier ouvert initialement en amiable) 
+Frais de gestion contentieux de 150€ HT (obtention du titre exécutoire, hors frais de greffe et de commissaire de justice) 
+Honoraires : 
+10 % des sommes encaissées pour les créances de moins de 6 mois 
+15 % des sommes encaissées pour les créances de moins de 6 mois 
+ 
+Nos honoraires sont déduits avant le reversement des montants récupérées sur les créances et les fonds récupérés vous sont reversés sous 48h afin d’optimiser leur trésorerie 
+ 
+Passage à l’adresse du débiteur (au choix du client) : 
+Forfait 50€ HT/passage  
+Frais kilométriques pris en charge par le client 
+      
+      
+      Ma démarche en ligne
+      </p>`,
+      background: 'assets/dossier.webp'
     },
     {
-      title: 'FAIRE EXÉCUTER UNE DÉCISION PAR COMMISSAIRE DE JUSTICE',
+      title: 'FAIRE EXÉCUTER UNE DÉCISION PAR UN COMMISSAIRE DE JUSTICE',
       description: 'Remise officielle d’actes par un commissaire de justice. Procédures maîtrisées et rapides.',
-      title2: 'Comment ça marche ?',
-      description2: 'Vous êtes titulaire d’une décision de justice (jugement, arrêt, ordonnance) et souhaitez contraindre celui qui a été condamné à satisfaire à ses obligations ? Sachez qu’il existe plusieurs procédures d’exécution afin de l’obliger à s’exécuter.',
-      title3: 'Procédure',
-      description3: 'Sachez qu’il existe plusieurs procédures d’exécution afin d’obliger la personne condamnée à s’exécuter. La mise en œuvre des procédures civiles d’exécution est le monopole de l’huissier de justice qui est garant de leur mise en place et de leur bon déroulement. En voici une liste non exhaustive :',
-      list : [ 'la saisie-attribution, qui permet de saisir tous les comptes bancaires que détient le débiteur ou toute somme d’argent devant lui revenir entre les mains de tiers',
-        'la saisie-vente, qui permet de dresser l’inventaire des biens mobiliers appartenant au débiteur pour procéder à leur vente le cas échéant',
-        'la saisie des rémunérations, qui permet de saisir une partie du salaire du débiteur',
-        'la saisie des biens immobiliers, qui permet de saisir un bien immobilier appartenant au débiteur',
-        'la saisie des véhicules terrestres à moteur,  qui permet de saisir notamment la voiture, la moto etc… dont pourrait être propriétaire le débiteur.',
-        'la saisie-appréhension et la saisie-revendication, qui donnent la possibilité au propriétaire d’un bien détenu par le débiteur d’en obtenir la restitution.'
-      ],
-      title4: 'Ma démarche en ligne',
-      description4: '',
+      html: `<p>Remise officielle d’actes par un commissaire de justice. Procédures maîtrisées et rapides.
+      Comment ça marche ?
+      Vous êtes titulaire d’une décision de justice (jugement, arrêt, ordonnance) et souhaitez contraindre celui qui a été condamné à satisfaire à ses obligations ? Sachez qu’il existe plusieurs procédures d’exécution afin de l’obliger à s’exécuter.
+      Procédure
+      Sachez qu’il existe plusieurs procédures d’exécution afin d’obliger la personne condamnée à s’exécuter. La mise en œuvre des procédures civiles d’exécution est le monopole de l’huissier de justice qui est garant de leur mise en place et de leur bon déroulement. En voici une liste non exhaustive :
+      la saisie-attribution, qui permet de saisir tous les comptes bancaires que détient le débiteur ou toute somme d’argent devant lui revenir entre les mains de tiers
+        la saisie-vente, qui permet de dresser l’inventaire des biens mobiliers appartenant au débiteur pour procéder à leur vente le cas échéant
+        la saisie des rémunérations, qui permet de saisir une partie du salaire du débiteur
+        la saisie des biens immobiliers, qui permet de saisir un bien immobilier appartenant au débiteur
+        la saisie des véhicules terrestres à moteur,  qui permet de saisir notamment la voiture, la moto etc… dont pourrait être propriétaire le débiteur.
+        la saisie-appréhension et la saisie-revendication, qui donnent la possibilité au propriétaire d’un bien détenu par le débiteur d’en obtenir la restitution.
+     Ma démarche en ligne</p>`,
       background: 'assets/justice.webp'
     }
   ];
